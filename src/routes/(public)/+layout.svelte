0d430a2e3a898d479bc53bba65e653caccb88e06
Siim <46551195+siim-m@users.noreply.github.com>
2022-12-18 16:12:32 +1100

    meta tags

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import Footer from '$lib/footer.svelte';
   import { initActiveViewsStore } from '$lib/views';
   import { setContext } from 'svelte';
@@ -7,6 +7,30 @@
   const activeViews = initActiveViewsStore();
   setContext('activeViews', activeViews);
 </script>
+
+<svelte:head>
+  <title>Moscow Time</title>
+  <meta
+    name="twitter:title"
+    content="Moscow Time"
+  />
+  <meta
+    name="twitter:card"
+    content="summary_large_image"
+  />
+  <meta
+    name="twitter:description"
+    content="The best bitcoin meme of 2021"
+  />
+  <meta
+    name="description"
+    content="The best bitcoin meme of 2021"
+  />
+  <meta
+    name="twitter:image"
+    content="https://moscowtime.xyz/card.png"
+  />
+</svelte:head>
 
 <div class="mx-2 flex h-full flex-col sm:mx-6 md:mx-10 lg:mx-14">
   <main class="flex-grow">
